9b8cb1fd04f944d7ab977c798c98fa7d7048d4db
tilakpatidar <tilakpatidar@gmail.com>
2015-10-20 15:00:57 +0530

    correct regex was taking 'I' as abbr

--- routes/regexes.js
+++ routes/regexes.js
@@ -1,9 +1,10 @@
 var store={
 	"acc_period":/([a-zA-Z]\.)+(([a-zA-Z]\s)|([a-zA-Z]\.))/g,
-	"acc_no_period":/([A-Z]+?)\s/g,
-	"acc_two_to_five_words_bw":/[a-zA-Z]{2,5}\.\s/g,
-	"acc_two_to_five_words_bw_dot_first":/\.[a-zA-Z]{2,5}\s/g,
+	"acc_no_period":/([A-Z]{2,}?)\s/g,
+	"acc_two_to_five_letter_bw":/[a-zA-Z]{2,5}\.\s/g,
+	"acc_two_to_five_letter_bw_dot_first":/\.[a-zA-Z]{2,5}\s/g,
 	"acc_single_dot":/[a-zA-Z]\.\s/g,
-	"acc":/(([a-zA-Z]\.)+(([a-zA-Z]\s)|([a-zA-Z]\.)))|(([A-Z]+?)\s)|([a-zA-Z]{2,5}\.\s)|(\.[a-zA-Z]{2,5}\s)|([a-zA-Z]\.\s)/g
+	"acc_jr's":/[a-zA-Z]{2}\.'s/g,
+	"acc":/(([a-zA-Z]\.)+(([a-zA-Z]\s)|([a-zA-Z]\.)))|(([A-Z]{2,}?)\s)|([a-zA-Z]{2,5}\.\s)|(\.[a-zA-Z]{2,5}\s)|([a-zA-Z]\.\s)|([a-zA-Z]{2}\.'s)/g
 };
 exports.store=store;
