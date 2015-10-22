b3d961705c41bd49c52fee9bc7dac41879a5de4a
Ayush Aggarwal <ayush.agarwal7689@gmail.com>
2015-10-22 17:37:38 +0530

    more number formats  date update

--- routes/segmenter/date/date.js
+++ routes/segmenter/date/date.js
@@ -2,17 +2,31 @@ var app={};
 var reg=require("./regexes");
 app.init=function(query){
 		var m;
+		var q=query;
 		var dic={};
 		var li=[];
 		var startin=[];
 		var endin=[];
-		while(m=reg.store.num_date_format.exec(query))
+		while(m=reg.store.reverse_date_format.exec(q))
 		{
 				dic[m[0]]=[m.index,m.index+m[0].length]
+				var re=new RegExp(m[0],'g');
+				var mask=m[0].replace(/./g,'#');
+				q=q.replace(re,mask);
 		}
-		while(m=reg.store.reverse_date_format.exec(query))
+		while(m=reg.store.num_date_format.exec(q))
 		{
 				dic[m[0]]=[m.index,m.index+m[0].length]
+				var re=new RegExp(m[0],'g');
+				var mask=m[0].replace(/./g,'#');
+				q=q.replace(re,mask);
+		}
+		while(m=reg.store.mmdd_date_format.exec(q))
+		{
+				dic[m[0]]=[m.index,m.index+m[0].length]
+				var re=new RegExp(m[0],'g');
+				var mask=m[0].replace(/./g,'#');
+				q=q.replace(re,mask);
 		}
 		console.log(dic);
 		return dic;
