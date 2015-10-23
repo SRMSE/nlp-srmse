ff1ba53a443ba110ae24ce03d62b3a0854ef334a
rishavmedhi <rishav164@gmail.com>
2015-10-23 17:40:01 +0530

    changes currency.js

--- routes/segmenter/currency/currency.js
+++ routes/segmenter/currency/currency.js
@@ -4,7 +4,6 @@ app.init=function(query){
 		var m;
 		var q=query;
 		var dic={};
-		console.log(query);
 		while(m=reg.store.currency.exec(q))
 		{
 			dic[m[0]]=[m.index,m.index+m[0].length];
@@ -12,9 +11,7 @@ app.init=function(query){
 			var mask=m[0].replace(/./g,'#');
 			q=q.replace(re,mask);
 		}
-		console.log(q);			
 		dic["query"]=q;
-		console.log(dic);	
 		return dic;
 };
 exports.currency=app;
