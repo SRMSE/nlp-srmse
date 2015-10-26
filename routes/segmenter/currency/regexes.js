ad62a1023e8ba58a32dc4a1349e3f9f96ec813bc
rishavmedhi <rishav164@gmail.com>
2015-10-26 23:23:09 +0530

    improvements in regex and # query return

--- routes/segmenter/currency/regexes.js
+++ routes/segmenter/currency/regexes.js
@@ -1,6 +1,6 @@
 	var rules={
-	"currency":/((₡|(R\$)|(kr)|(Rp)|₩|₨|฿|(руб)|(RM)|R|₹|¥|€|£|\$)(\s*\d+(,\d+)*(\.\d+)?))|((\d+(,\d+)*(\.\d+)?\s*)(\$|rupees|rupee|£|€|¥|₹|(RM)|(руб)|฿|₨|₩|(Rp)|(kr)|(R\$)|R|₡|dollars|dollar|pounds|pound|baht))/gi,
-	"stform":/(₡|(R\$)|(kr)|(Rp)|₩|₨|฿|(руб)|(RM)|R|₹|¥|€|£|\$)/gi,
+	"currency":/(?=\s?)((₡|(R\$)|(kr)|(Rp)|Rs|₩|₨|฿|(руб)|(RM)|R|₹|¥|€|£|\$)(\.?)(\s*\d+(,\d+)*(\.\d+)?\s?(lakhs|lakh|million|billion|thousand)?))(?=\s)|((\d+(,\d+)*(\.\d+)?\s*(lakhs|lakh|million|billion|thousand)?)\s?(\$|rupees|rupee|£|€|¥|₹|(RM)|Rs|(руб)|฿|₨|₩|(Rp)|(kr)|(R\$)|R|₡|dollars|dollar|pounds|pound|baht))/gi,
+	"stform":/(₡|(R\$)|(Rs)|(kr)|(Rp)|₩|₨|฿|(руб)|(RM)|R|₹|¥|€|£|\$)/gi,
 	"no":/(\s*\d+(,\d+)*(\.\d+)?)/gi,
 	"comma":/,+/g,
 };
