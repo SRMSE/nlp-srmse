caf71c80394d51f529cbff7b7d9f5d41eaa90c7e
Devaraj Phukan <devaraj.phukan@gmail.com>
2015-10-29 11:25:17 +0530

    Distance unit regex

--- routes/segmenter/units/regexes.js
+++ routes/segmenter/units/regexes.js
@@ -1,2 +1,4 @@
-var regexes={};
-exports.store=regexes;
+	var rules={
+	"distance":/(\d+).?(\d*)\s*(m|metres?|cm|centi[- ]?metres?|kms?|kilo[- ]?metres?|dm|deci[- ]?metres?|nm|nano[- ]?metres?|angstrom|micro[- ]?metres?|microns?|miles?|mi|yards?|yd|foot|feet|ft|inch[es]?|in)/g
+};
+exports.store=rules;
